afc344f3760a425a66e174b1ee5fc2ab76f2ca29
AdityaANH <aditya.n.h02@gmail.com>
2022-03-18 17:39:35 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul4_1302204078/tpmodul4_1302204078/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302204078
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric<string> halo = new HaloGeneric<string>();
+            String x = "Aditya Nur Hidayatullah";
+            halo.SapaUser(x);
+        }
+    }
+    public class HaloGeneric<T>
+    {
+        public void SapaUser(T x)
+        {
+            Console.WriteLine("halo" + x);
+        }
+    }
+}
